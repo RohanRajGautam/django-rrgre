0d249593c19b734ea7d92509911e9ecbb05b1a49
rohan <rohanrajgautam@gmail.com>
2020-10-03 18:17:26 +0545

    add dynamic listings content to the home page

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
