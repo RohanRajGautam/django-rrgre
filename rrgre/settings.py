b70a9dbb554b13019ac81c14398a5266ead172f3
rohan <rohanrajgautam@gmail.com>
2020-10-03 17:44:14 +0545

    added background image

--- rrgre/settings.py
+++ rrgre/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'rrgre/static')
+    os.path.join(BASE_DIR, 'rrgre/static'),
 ]
 
 #Media Root Folder
