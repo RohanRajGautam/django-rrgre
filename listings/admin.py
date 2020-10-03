538f4a62b13247c1b61cab0f3de712f22897c6e6
rohan <rohanrajgautam@gmail.com>
2020-10-03 15:02:57 +0545

    customization of listing and realtor admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+  list_display_links = ('id', 'title')
+  list_filter = ('realtor',)
+  list_editable = ('is_published',)
+  search_fields = ('title', 'description', 'price', 'state', 'zipcode', 'city', 'address')
+  list_per_page = 20
+
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'email', 'phone', 'hire_date')
+  list_display_links = ('id', 'name')
+  search_fields = ('name',)
+  list_per_page = 20
+
+admin.site.register(Realtor, RealtorAdmin)
