edf7c8807d63b1a4ba4f48b970281ea97c993fec
rohan <rohanrajgautam@gmail.com>
2020-10-04 18:26:43 +0545

    dynamic navbar after login, and logout

--- accounts/views.py
+++ accounts/views.py
@@ -57,6 +57,8 @@ def login(request):
 
 def logout(request):
   if request.method == 'POST':
+    auth.logout(request)
+    messages.success(request, 'You are now logged out')
     return redirect('index')
 
 def dashboard(request):
