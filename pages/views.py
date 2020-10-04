94d254ac769340abf5c2f6e3241bbbd455cf147b
rohan <rohanrajgautam@gmail.com>
2020-10-04 07:59:39 +0545

    added choice file to loop in through option of select in frontpage search

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 # Create your views here.
 
@@ -9,7 +10,10 @@ from realtors.models import Realtor
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
-        'listings': listings
+        'listings': listings,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
     }
     return render(request, 'pages/index.html', context)
 
