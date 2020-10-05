42119af4dacf60283e6e8b0dd2fdf4083dc69174
rohan <rohanrajgautam@gmail.com>
2020-10-05 09:42:06 +0545

    new changes

--- rrgre/settings.py
+++ rrgre/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     }
 }
 
-postgresql-graceful-86873 = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(postgresql-graceful-86873)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
