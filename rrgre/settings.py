29c90e75ff9cb06e13f0e24e46eb8af7098bec19
rohan <rohanrajgautam@gmail.com>
2020-10-05 10:27:38 +0545

    new changes

--- rrgre/settings.py
+++ rrgre/settings.py
@@ -143,8 +143,3 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
